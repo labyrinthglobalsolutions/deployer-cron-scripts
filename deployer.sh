d27738096dfb9a3bf4dbfe81eda1a371f107102f
Meer Sawood <msawood@redhat.com>
2023-08-31 22:21:47 +0530

    added new changes

--- deployer.sh
+++ deployer.sh
@@ -18,6 +18,35 @@ mkdir -p "$LOG_DIR"
 # Redirect all output to the log file
 exec > >(tee -a "$LOG_DIR/script_log.txt") 2>&1
 
+check_image() {
+    tag="$1"
+    timeout=$((15 * 60))  # 30 minutes in seconds
+    elapsed_time=0
+    quay_repo=$(basename "$2")
+    quay_api="https://quay.io/api/v1/repository/$QUAY_ORG/$quay_repo/tag/?onlyActiveTags=true"
+
+    while [ $elapsed_time -lt $timeout ]; do
+        response=$(curl -s "$quay_api")
+        image_found=$(echo "$response" | jq -r ".tags[] | select(.name == \"$tag\")")
+
+        if [ -n "$image_found" ]; then
+            echo "Docker image found for tag $tag"
+            git config --global --add safe.directory $local_dir
+            sleep 5
+            # Trigger the redeploy script with sudo
+            sudo ./redeploy.sh
+            return 0  # Return from the function, continuing the parent loop
+        else
+            echo "Docker image not found for tag $tag. Waiting for 2 minutes..."
+            sleep 120  # Wait for 2 minutes
+            elapsed_time=$((elapsed_time + 120))
+        fi
+    done
+
+    echo "Timeout reached. Docker image not found for tag $tag on Quay.io after 30 minutes."
+    return 1  # Return from the function, continuing the parent loop
+}
+
 # Loop through each repository and perform actions
 for repo_info in "${REPOSITORIES[@]}"; do
     IFS='|' read -ra repo_data <<< "$repo_info"
@@ -43,33 +72,13 @@ for repo_info in "${REPOSITORIES[@]}"; do
         git pull origin main
         echo "$repo_url: Pull complete."
 
-        tag=$latest_commit
-        timeout=$((15 * 60))  # 30 minutes in seconds
-        elapsed_time=0
-        quay_repo=$(basename "$local_dir")
-        quay_api="https://quay.io/api/v1/repository/$QUAY_ORG/$quay_repo/tag/?onlyActiveTags=true"
-
-        while [ $elapsed_time -lt $timeout ]; do
-            response=$(curl -s "$quay_api")
-            image_found=$(echo "$response" | jq -r ".tags[] | select(.name == \"$tag\")")
-
-            if [ -n "$image_found" ]; then
-                echo "Docker image found for tag $tag"
-                 # Sleep for 5 seconds
-                git config --global --add safe.directory $local_dir
-                sleep 5
-                # Trigger the redeploy script with sudo
-                sudo ./redeploy.sh
-
-            else
-                echo "Docker image not found for tag $tag. Waiting for 2 minutes..."
-                sleep 120  # Wait for 2 minutes
-                elapsed_time=$((elapsed_time + 120))
-            fi
-        done
-
-        echo "Timeout reached. Docker image not found for tag $tag on Quay.io after 30 minutes."
-        exit 1
+        check_image "$tag" "$local_dir"
+        if [ $? -eq 0 ]; then
+            echo "Continuing with the next iteration of the parent loop..."
+        else
+            echo "Image not found for tag $tag"
+        fi
+        continue
         
     else
         echo "$repo_url: No changes detected."
